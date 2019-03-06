1a10104df63bf8072eda3c7e852f425bd7f6209b
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-06 02:32:51 +0000

    added app to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
+const app = require("./app")
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 15454;
 
 const server = http.createServer();
 
